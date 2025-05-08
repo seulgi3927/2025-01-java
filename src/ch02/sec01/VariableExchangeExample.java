8e52c530bd4eec008466e61a275499ec5a301779
seulgi3927 <seulgi3927@gmail.com>
2025-05-08 18:10:05 +0900

    first_1

---
+++ src/ch02/sec01/VariableExchangeExample.java
@@ -0,0 +1,15 @@
+package ch02.sec01;
+
+public class VariableExchangeExample {
+    // 메인 메소드 작성
+    public static void main(String [] args) {
+        int x = 3, y = 5;
+        System.out.println("x: " + x + ", y: " + y); // x: 3, y: 5
+
+        int temp = x; // 빈 공간에다 x의 값을 저장해둠
+        x = y;
+        y = temp;
+
+        System.out.println("x: " + x + ", y: " + y); // x: 5, y: 3
+    }
+}
